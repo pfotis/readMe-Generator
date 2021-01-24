aae8581c1fc0908b327d9a24aab79f5f46a0bc94
pfotis <69961508+pfotis@users.noreply.github.com>
2021-01-25 08:37:26 +1100

    npm install inquirer

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'checkbox',
+    message: 'What languages do you know?',
+    name: 'stack',
+    choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+  },
+  {
+    type: 'list',
+    message: 'What is your preferred method of communication?',
+    name: 'contact',
+    choices: ['email', 'phone', 'telekinesis'],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
