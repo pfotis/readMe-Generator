1906b8cbdfc3c9e29e5609b763a4c74114cf9e1c
pfotis <69961508+pfotis@users.noreply.github.com>
2021-01-25 08:46:54 +1100

    add some more questions

--- index.js
+++ index.js
@@ -3,23 +3,58 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [{
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?",
+  },
+  {
     type: 'input',
     name: 'name',
     message: 'What is your name?',
   },
   {
-    type: 'checkbox',
-    message: 'What languages do you know?',
-    name: 'stack',
-    choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
   },
   {
-    type: 'list',
-    message: 'What is your preferred method of communication?',
-    name: 'contact',
-    choices: ['email', 'phone', 'telekinesis'],
+    type: 'input',
+    name: 'github',
+    message: 'What is your github?',
   },
+  {
+    type: 'checkbox',
+    message: 'What languages do you use?',
+    name: 'stack',
+    choices: [
+        'HTML',
+        'CSS',
+        'JavaScript', 
+        'jQuery',
+        'nodejs',
+        'MySQL',
+        ]
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What license will you use in your project?',
+    choices: [
+        'MIT',
+        'Unlicense',
+        'Apache 2.0',
+        'GNU v3',
+        'BSD 3-Clause',
+        'Mozilla Public License 2.0'
+        ]
+  },
+  {
+    type: 'input',
+    name: 'shortDescription',
+    message: 'Please write a short decription of youy project:',
+  }
 ];
 
 // TODO: Create a function to write README file
